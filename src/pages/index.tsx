4bd20c6440e139f6f35f6acb9d7dc1fede84b4b0
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-08-21 18:33:30 +0530

    added footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { NextSeo } from "next-seo";
 import { useEffect } from "react";
 import { db } from "../../firebase";
 import Banner from "../components/Banner";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Items from "../components/Items";
 
@@ -90,6 +91,7 @@ const Home: React.FC = () => {
           </motion.div>
         </div>
       </main>
+      <Footer />
     </div>
   );
 };
