56faef12006f7c32b9e3796be01b8a45979f5a9f
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-08-24 19:32:19 +0530

    made header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ function Header() {
     >
       <header className="flex items-center my-2 justify-between px-10 text-text">
         <Link href="/">
-          <a className="hidden md:inline-flex">
+          <a className="hidden sm:inline-flex">
             <Image
               width={100}
               height={100}
@@ -39,34 +39,35 @@ function Header() {
             />
           </a>
         </Link>
+        <div className="hidden sm:flex items-center space-x-4">
+          <motion.div
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 0.5 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            className="space-x-6 text-xl font-medium hover:underline"
+          >
+            <Link href="/orders">
+              <a>My orders</a>
+            </Link>
+          </motion.div>
+
+          <motion.div
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 0.5 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            className="space-x-6 text-xl font-medium hover:underline"
+          >
+            <Link href="/parcel/orders">
+              <a>Parcel tracking</a>
+            </Link>
+          </motion.div>
+        </div>
         <div className="items-center hidden sm:flex">
           <div className="flex items-center mt-2 ml-4 space-x-8 justify-evenly">
-            <motion.div
-              whileHover={{
-                scale: 1.2,
-                transition: { duration: 0.5 },
-              }}
-              whileTap={{ scale: 0.9 }}
-              className="space-x-6 text-xl font-medium hover:underline"
-            >
-              <Link href="/orders">
-                <a>My orders</a>
-              </Link>
-            </motion.div>
-
-            <motion.div
-              whileHover={{
-                scale: 1.2,
-                transition: { duration: 0.5 },
-              }}
-              whileTap={{ scale: 0.9 }}
-              className="space-x-6 text-xl  font-medium hover:underline"
-            >
-              <Link href="/parcel/orders">
-                <a>Parcel tracking</a>
-              </Link>
-            </motion.div>
-
             <motion.div
               whileHover={{
                 scale: 1.2,
@@ -100,9 +101,9 @@ function Header() {
         </div>
 
         <Link href="/cart" passHref>
-          <div className="md:hidden relative inline-flex ">
-            <ShoppingBagIcon className="w-12 h-12 p-3 mr-3 bg-white rounded-full md:hidden backdrop-filter backdrop-blur-2xl bg-opacity-40" />
-            <h2 className="backdrop-filter shadow-xl absolute right-0 top-0 -mt-1 bg-white bg-opacity-75 mr-2 text- font-semibold cursor-pointer md:hidden inline-flex rounded-full  px-[8px]">
+          <div className="sm:hidden relative inline-flex ">
+            <ShoppingBagIcon className="w-12 h-12 p-3 mr-3 bg-white rounded-full sm:hidden backdrop-filter backdrop-blur-2xl bg-opacity-40" />
+            <h2 className="backdrop-filter shadow-xl absolute right-0 top-0 -mt-1 bg-white bg-opacity-75 mr-2 text- font-semibold cursor-pointer sm:hidden inline-flex rounded-full  px-[8px]">
               {items.length}
             </h2>
           </div>
