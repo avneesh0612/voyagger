43b045ec13c64c5cba7951356e59a31ab771630e
Your Name <avneeshagarwal0612@gmail.com>
2021-08-31 15:09:38 +0530

    still testing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -8,6 +9,17 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/favicon.svg"></link>
           <meta name="theme-color" content="#fff" />
+
+          <link
+            href="https://chatbotish.vercel.app/index.css"
+            rel="stylesheet"
+          />
+          <Script
+            src="https://chatbotish.vercel.app/index.js"
+            data-chatbotish
+            data-id="HD78EFfUJSI7sFbTdfnO"
+            strategy="lazyOnload"
+          ></Script>
         </Head>
         <body>
           <Main />
